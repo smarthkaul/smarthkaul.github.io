7c04360ef6b9ddad8b0838ca7c8ff4b81601ef1a
smarthkaul <105581386+smarthkaul@users.noreply.github.com>
2026-07-08 23:08:28 -0400

    Apply turf background, wire skills ticker, reskin 404 and meta

--- website/src/pages/Layout.jsx
+++ website/src/pages/Layout.jsx
@@ -2,16 +2,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
-};
+const Layout = () => (
+  <>
+    <Navbar />
+    <main className="court-turf min-h-screen">
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
 
 export default Layout;
